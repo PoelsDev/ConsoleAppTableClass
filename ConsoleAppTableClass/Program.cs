6817f943063a03c22dd7370764eff15cd321d32f
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-02-05 09:23:19 +0100

    1ste versie

--- ConsoleAppTableClass/Program.cs
+++ ConsoleAppTableClass/Program.cs
@@ -9,6 +9,23 @@ namespace ConsoleAppTableClass
     {
         static void Main(string[] args)
         {
+            Random rndGen = new Random();
+            // Table t = new Table(rndGen.Next(50, 200), rndGen.Next(50, 200));
+            //t.ShowData();
+
+            List<Table> Tables = new List<Table>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int w = rndGen.Next(50, 200);
+                int h = rndGen.Next(50, 200);
+                Tables.Add(new Table(w, h));
+            }
+
+            foreach (var t in Tables)
+            {
+                t.ShowData();
+            }
         }
     }
 }
---
+++ ConsoleAppTableClass/Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleAppTableClass
+{
+    
+    class Table
+    {
+        private int height = 20;
+        private int width = 40;
+        public Table(int width, int height)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Width: " + width + " cm");
+            Console.WriteLine("Height: " + height + " cm");
+            Console.WriteLine();
+        }
+    }
+
+
+
+}
